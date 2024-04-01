87acb8c86982b0c0961b6198f127feaa30a8653c
abisheksonu <142437877+abisheksonu@users.noreply.github.com>
2024-04-01 09:55:51 +0530

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import os
+
+
+import google.generativeai as genai
+## Configure Genai Key
+
+genai.configure(api_key='AIzaSyBaHfRevV8XEaM34f6iQ6_XN2F63_BRHJE')
+
+## Function To Load Google Gemini Model and provide queries as response
+
+def get_gemini_response(question,prompt):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content([prompt[0],question])
+    return response.text
+
+## Fucntion To retrieve query from the database
+
+
+
+## Define Your Prompt
+prompt=[
+    """
+    You are an expert in converting English questions to SQL query!
+    The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
+    SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
+    the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
+    \nExample 2 - Tell me all the students studying in Data Science class?, 
+    the SQL command will be something like this SELECT * FROM STUDENT 
+    where CLASS="Data Science"; 
+    also the sql code should not have ``` in beginning or end and sql word in output
+
+    """
+
+
+]
+
+## Streamlit App
+
+st.set_page_config(page_title="I can Retrieve Any SQL query")
+st.header("Gemini App To Retrieve SQL Data")
+
+question=st.text_input("Input: ",key="input")
+
+submit=st.button("Ask the question")
+
+# if submit is clicked
+if submit:
+    response=get_gemini_response(question,prompt)
+    st.write(response)
+    
+
+
+
+
+
+
+
+
+
